33f619602085fd05d7c055ce620b0e3f156a4032
Gustavo Henrique <gustavo.barbosa@ufpr.br>
2022-08-19 23:28:41 -0300

    finalmente a 10 a

--- semana10/a/program.cpp
+++ semana10/a/program.cpp
@@ -2,19 +2,52 @@
 
 using namespace std;
 using ll = long long;
-
-int main() {
+const int L = log2(1123456789)+1;
+int main()
+{
     cin.tie(0);
     ios_base::sync_with_stdio(0);
     // implemente sua lógica
 
     ll n, q;
     cin >> n >> q;
-    vector<vector<int>> planetas;
 
+    vector<vector<int>>planetas(L+1, vector<int>(n+1));
+    
     // Monta
-    for(ll u = 0; u < q; u++){
-        int v; cin >> v;
+    for (ll u = 1; u <= n; u++) {
+        ll v;
+        cin >> v;
+        // v--;
+        planetas[0][u] = v;
     }
 
+    // Preenche
+    for(ll i = 1; i <= L; i++){
+        for(ll j = 1; j<= n; j++){
+            planetas[i][j] = planetas[i-1][(planetas[i-1][j])];
+        }
+    }
+
+    // for(ll i = 1; i <= n; i++){
+    //     cout << i << " > ";
+    //     for(ll j = 1; j <= n; j++){
+    //         cout << planetas[i][j] << " |";
+    //     }
+    //     cout << endl;
+    // }
+
+    // Faz as consultas
+    for(int i = 0; i < q; i++){
+        int ori, dist;
+        cin >> ori >> dist;
+
+        int planeta = ori;
+        for(int i = L; i >= 0; i--) {
+            if(dist & (1 << i)){
+                planeta = planetas[i][planeta];
+            }
+        }
+        cout << planeta << "\n";
+    }
 }
